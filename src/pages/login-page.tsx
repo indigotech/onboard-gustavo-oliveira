73cbb867b4c7f0eb197d96cea58398e61a297f45
GustavoOM <gustavo.oliveira@taqtile.com.br>
2025-05-08 11:55:08 -0300

    PR resolve

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -5,7 +5,7 @@ import {MainInput} from '../components/main-input';
 
 export const LoginPage: React.FC = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text style={styles.title}>Bem-vindo(a) à Taqtile!</Text>
       <MainInput title="E-mail" />
       <MainInput title="Senha" />
@@ -15,14 +15,8 @@ export const LoginPage: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-  },
   title: {
     fontSize: 24,
-    marginBottom: 20,
+    marginVertical: 20,
   },
 });
